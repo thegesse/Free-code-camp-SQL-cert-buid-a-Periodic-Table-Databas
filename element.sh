fec2aabda9220488c0ace1350cc69f5ca5a39502
Esteban Quillerier <emquillerier@gmail.com>
2026-09-22 12:39:56 -0400

    feat: pulling from database to make a sentence

--- element.sh
+++ element.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
+PSQL="psql --username=freecodecamp --dbname=<periodic_table> -t --no-align -c"
 
-arg=$1
-
-if [[ -z "$arg" ]]; then
+if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit 0
 fi
+
+if [[ $1 = ^[0-9]+$ ]]; then
+  WHERE_CLAUSE="e.atomic_number = $1"
+else
+  WHERE_CLAUSE="e.symbol = '$1' OR e.name = '$1'"
+fi
+
+RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass,p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $WHERE_CLAUSE;")
+
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
+
+echo "the element with the atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+
