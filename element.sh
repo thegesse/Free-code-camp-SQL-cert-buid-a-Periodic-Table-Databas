e4b7fbf00dc6045d9c4c6eb2e353e7132da76c23
Esteban Quillerier <emquillerier@gmail.com>
2026-09-22 12:46:19 -0400

    feat: added case for when element isnt in the database

--- element.sh
+++ element.sh
@@ -14,6 +14,11 @@ fi
 
 RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass,p.melting_point_celsius, p.boiling_point_celsius FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE $WHERE_CLAUSE;")
 
-IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
+if [[ -z $RESULT ]]; then
+  echo "I could not find that element in the database."
+  exit 0
+fi
 
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$RESULT"
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+
